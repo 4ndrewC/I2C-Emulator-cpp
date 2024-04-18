cb4beeaf0333b3a51d050461eb3d97ae9e425d87
4ndrewC <84311342+4ndrewC@users.noreply.github.com>
2024-04-18 11:49:23 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,95 +1,109 @@
-#include "I2C.cpp"
+#include "master.cpp"
 
-class slave: public I2C{
-    public:
-        //registers
-        u8 idx = 0;
-        u8 drx = 0;
-        u8 rrx = 0;
-        u8 state = 1;
+I2C line1;
 
-        vector<u8> addr, received_addr, data;
+void print_lines(){
+    cout<<"SDA Line: ";
+    for(u8 i : line1.SDA) cout<<(int)i<<" ";
+    cout<<endl;
+    cout<<"SCL Line: ";
+    for(u8 i: line1.SCL) cout<<(int)i<<" ";
+    cout<<endl;
+}
 
-        //bits
-        u8 rec = 1;
-        u8 rw;
+void run(master &master1, master &master2, vector<u8> bitstream){
+    cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+    //transmission starts here
+    master1.inputstream(bitstream);
+    line1.check_startcond();
+    print_lines();   
+    cout<<"Start: "<<(int)line1.start<<endl;
+    for(int i = 0; i<ADDRSIZE; i++){
+        master1.send_data(bitstream[i]);
+        master2.receive();
+        
+        print_lines();
+        
+    }
+    cout<<"Rec for slave 1 -------------------------"<<(int)master2.rec<<endl;
 
-        slave(vector<u8> &a){
-            addr.resize(ADDRSIZE);
-            received_addr.resize(ADDRSIZE);
-            data.resize(DATASIZE);
-            for(int i = 0; i<ADDRSIZE; i++){
-                addr[i] = a[i];
-            }
+    //send read/write bit
+    master1.send_data(bitstream[RWLOC]);
+    master2.receive();
+    // slave1.receive();
+    print_lines();
+    cout<<"rw bit: "<<(int)master2.rw<<endl;
+    //ack bit
+    cout<<"send ack bit: "<<endl;
+    master2.sendack();
+    print_lines();
+
+    //data bits
+    for(int i = DATALOC; i<DATALOC+DATASIZE; i++){
+        master1.send_data(bitstream[i]);
+        master2.receive();
+        print_lines();
+    }
+
+    //release SDA line
+    cout<<"release SDA"<<endl;
+    master1.release();
+    master2.receive();
+    print_lines();
+    line1.check_stopcond();
+    cout<<"Stop: "<<(int)line1.stop<<endl;
+    cout<<"Start: "<<(int)line1.start<<endl;
+    
+
+    // ---------------------------------------------------------------------
+    //reset everything for next transmission
+    master2.state = 1;
+    master2.idx = 0;
+    master2.rec = 1;
+    for(int i = 0; i<ADDRSIZE; i++){
+        master2.received_addr[i] = 0;
+    }
+    for(int i = 0; i<DATASIZE; i++){
+        master2.data[i] = 0;
+    }
+}
+
+int main(){
+    //create I2C line
+    line1 = I2C();
+
+    """sample usage""";
+
+    //create master
+    vector<u8> master1addr = {1,1,0,1,1,1,0};
+    master master1(master1addr);
+
+    vector<u8> plcaddr = {0,1,1,1,0,0,0};
+    master plc(plcaddr);
+
+    
+    //programmables
+    string sample_message = "1EA4rhgo8792mGP";
+    vector<u8> targetaddr = {1,1,0,1,1,1,0};
+    string received_message = "";
+    //dont need to write here
+    u8 readwrite = 1;
+    for(int i = 0; i<sample_message.length(); i++){
+        vector<u8> curbitstream(16);
+        for(int j = 0; j<ADDRSIZE; j++) curbitstream[j] = targetaddr[j];
+        curbitstream[RWLOC] = readwrite;
+        u8 byte = (u8)sample_message.substr(i,1)[0];
+        //reverse because protocol is big endian
+        vector<u8> rev(DATASIZE);
+        for(int j = 0; j<DATASIZE; j++){
+            rev[j] = byte&1;
+            byte>>=1;
         }
+        for(int j = 0; j<DATASIZE; j++) curbitstream[DATALOC+DATASIZE-j-1] = rev[j];
+        run(plc, master1, curbitstream);
+        received_message+= (char)master1.drx;
+    }
+    cout<<received_message<<endl;
 
-        u8 checkaddr(){
-            for(int i = 0; i<ADDRSIZE; i++){
-                if(addr[i]!=received_addr[i]){
-                    return 0;
-                }
-            }
-            return 1;
-        }
-
-        void sendack(){
-            sda_send(1);
-            tick();
-        }
-
-        void receive(){
-            u8 bit = SDA[WINDOW-1];
-            if(!rec) return;
-            if(start){
-                if(state&(1<<0)){
-                    cout<<"state 1"<<endl;
-                    received_addr[idx] = bit;
-                    int temp = (int)idx;
-                    ++idx;
-                    if(idx==ADDRSIZE){
-                        idx = 0;
-                        //move to next state
-                        rec = checkaddr();
-                        if(!rec){
-                            cout<<"Address not matched"<<endl;
-                            return;
-                        }
-                        state<<=1;
-                    }
-                }
-                else if(state&(1<<1)){
-                    cout<<"state 2"<<endl;
-                    rw = bit;
-                    idx = 0;
-                    state<<=1;
-                }
-                else if(state&(1<<2)){
-                    cout<<"state 3"<<endl;
-                    data[idx] = bit;
-                    drx = (drx<<1) | static_cast<int>(bit);
-                    int temp = (int)idx;
-                    ++idx;
-                    if(idx==DATASIZE){
-                        idx = 0;
-                        state<<=1;
-                        cout<<"data: "<<(int)drx<<endl;
-                        if(rw){
-                            rrx = EEPROM[drx];
-                            cout<<"read data: "<<(int)drx<<endl;
-                        }
-                        else{
-                            EEPROM[memptr] = drx;
-                            ++memptr;
-                            cout<<"wrote data: "<<(int)drx<<" into mem location: "<<memptr-1<<endl;
-                        }
-                    }
-                }
-                else if(state&(1<<3)){
-                    cout<<"state 4"<<endl;
-
-                }
-                
-
-            }
-        }
-};
+    return 0;
+}
